4573523c00a9bf10c2b3e550e939c9a282734b58
Hadson00 <hadson798@gmail.com>
2024-07-01 22:14:34 -0300

    cart

--- MyProject/myapp/urls.py
+++ MyProject/myapp/urls.py
@@ -9,4 +9,9 @@ urlpatterns = [
     path("create/", create, name="create_game"),
     path("edit/<int:game_id>", edit, name="edit_game"),
     path("delete/<int:game_id>", delete_game, name="delete_game"),
+    path('cart/', view_cart, name='view_cart'),
+    path('add_to_cart/<int:game_id>', add_to_cart, name='add_to_cart'),
+    path('remove_from_cart/<int:game_id>', remove_from_cart, name='remove_from_cart'),
+    path('update_cart/<int:game_id>', update_cart, name='update_cart'),
+    path('checkout/', checkout, name='checkout'),
 ]
--- MyProject/myapp/views.py
+++ MyProject/myapp/views.py
@@ -3,7 +3,7 @@ from .models import *
 from .forms import *
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-
+from django.http import JsonResponse
 
 @login_required
 def home(request):
@@ -73,3 +73,42 @@ def delete_game(request, game_id):
         messages.success(request, 'Jogo deletado com sucesso!')
         return redirect('home')
     return render(request, 'site/delete.html', {'game': game})
+
+def add_to_cart(request, game_id):
+    game = get_object_or_404(Game, id = game_id)
+    cart, created = Cart.objects.get_or_create(user=request.user)
+    cart_item, created = CartItem.objects.get_or_create(cart=cart, game=game, quantity = 1)
+    if not created:
+        cart_item.quantity += 1
+        cart_item.save()
+    return JsonResponse({'succes':'true', 'quantity': cart_item.quantity})
+
+def remove_from_cart(request, game_id):
+    cart_item = get_object_or_404(CartItem, id=game_id)
+    cart_item.delete()
+    return JsonResponse({'succes':'true'})
+
+def view_cart(request):
+    cart, created = Cart.objects.get_or_create(user=request.user)
+    items = CartItem.objects.filter(cart=cart)
+    return render(request, 'site/cart.html', {'cart':cart, 'items':items})
+
+def update_cart(request, game_id):
+    cart_item = get_object_or_404(CartItem,id=game_id)
+    
+    if request.method == 'POST':
+        action = request.POST.get('action')
+        if action == 'increase':
+            cart_item.quantity += 1
+        elif action == 'decrease' and cart_item.quantity > 1:
+            cart_item.quantity -= 1
+        cart_item.save()
+        return JsonResponse({'succes':'true', 'quantity':cart_item.quantity})
+
+def checkout(request):
+    cart, created = Cart.objects.get_or_create(user=request.user)
+    if request.method == 'POST':
+        cart.delete()
+        messages.succes(request,'Compra realizada com sucesso')
+        return redirect('index')
+    return render(request, 'site/checkout.html', {'cart':cart})
